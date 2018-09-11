977c282bb0c91b5944c954a05659cfc732208b44
Isaac <ikehunter5@gmail.com>
2018-09-11 04:39:55 -0400

    add update file/scripts

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp')
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b9245130941d2ebe107c798')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b90f2ebeafeeb3224c63595')
+  }, {
+    $set: {
+      name: "Isaac"
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  })
+
+  // client.close();
+});
