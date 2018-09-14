39ecbc6b6cd6ad243ce7af1fc58b43e322911c20
Isaac <ikehunter5@gmail.com>
2018-09-14 05:29:38 -0400

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -28,6 +30,29 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    // return insures it doesnt run rest of code
+    return res.status(404).send('Not a valid id');
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      //return console.log('Id not found');
+    return res.status(404).send('No todos found');
+    }
+
+    res.send({todo});
+    //uses {} because it sends it as object, later i cound add more things
+
+  }).catch((e) => {
+    res.status(404).send('Something went wrong');
+  })
+
+})
 
 app.listen(3000, () => {
   console.log('Live on port 3000');
